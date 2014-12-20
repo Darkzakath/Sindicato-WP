13f2150fc446294d1751653ae178d5282007cdc0
Ezequiel Toloza <ezequiel.toloza@gmail.com>
2014-12-20 20:40:13 -0300

    cambios en config

--- wp-config.php
+++ wp-config.php
@@ -16,13 +16,13 @@
 
 // ** Ajustes de MySQL. Solicita estos datos a tu proveedor de alojamiento web. ** //
 /** El nombre de tu base de datos de WordPress */
-define('DB_NAME', 'nombredetubasededatos');
+define('databasewp', 'nombredetubasededatos');
 
 /** Tu nombre de usuario de MySQL */
-define('DB_USER', 'nombredeusuario');
+define('root', 'nombredeusuario');
 
 /** Tu contraseña de MySQL */
-define('DB_PASSWORD', 'contraseña');
+define('', 'contraseña');
 
 /** Host de MySQL (es muy probable que no necesites cambiarlo) */
 define('DB_HOST', 'localhost');
